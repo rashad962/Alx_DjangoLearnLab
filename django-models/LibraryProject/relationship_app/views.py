ad32d04cdb7ed3de9132897c2ccb50cf6c9091ed
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-04 17:48:20 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,29 +1,14 @@
-<<<<<<< HEAD
-# Import necessary modules
-=======
-# relationship_app/views.py
->>>>>>> 7807924 (vuje)
 from django.shortcuts import render
 from django.views.generic import DetailView
-<<<<<<< HEAD
-from .models import Book, Library  # Ensure Library is imported here
-=======
-from .models import Book, Library  # Import Library model here
->>>>>>> 092f12d7c74663fce420e898c048019cd45c03eb
+from .models import Book, Library
 
-# Function-Based View to List All Books
+# Function-based view to list all books
 def list_books(request):
-    books = Book.objects.all()
-    return render(request, "relationship_app/list_books.html", {"books": books})
+    books = Book.objects.select_related('author').all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-Based View to Display Library Details
+# Class-based view to show library details
 class LibraryDetailView(DetailView):
-<<<<<<< HEAD
     model = Library
-    template_name = "relationship_app/library_detail.html"
-    context_object_name = "library"
-=======
-    model = Library  # Set the model to Library to show details of a specific library
-    template_name = "relationship_app/library_detail.html"  # Template for the class-based view
-    context_object_name = "library"  # The context variable passed to the template
->>>>>>> 092f12d7c74663fce420e898c048019cd45c03eb
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
