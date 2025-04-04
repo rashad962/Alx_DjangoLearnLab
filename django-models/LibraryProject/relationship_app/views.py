b438bf84655ced26edcd1d97a206a1608a417a94
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-04 21:06:52 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib import messages
 from .models import Book, Author
