bbd281cd8bb506ef6010db2e9ada0423a4f08bfb
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-05 17:53:11 +0200

    Update admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,23 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import GroupAdmin
-from django.contrib.auth.models import Group, Permission
-from .models import Book, CustomUser
+from django.contrib.auth.models import Group
+from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'published_date', 'is_available')
     list_filter = ('is_available',)
     search_fields = ('title', 'author')
 
-class CustomGroupAdmin(GroupAdmin):
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        form.base_fields['permissions'].queryset = Permission.objects.filter(
-            content_type__app_label__in=['bookshelf', 'auth']
-        )
-        return form
-
-# Unregister the default Group admin and register our custom one
-admin.site.unregister(Group)
-admin.site.register(Group, CustomGroupAdmin)
 admin.site.register(Book, BookAdmin)
-admin.site.register(CustomUser)
