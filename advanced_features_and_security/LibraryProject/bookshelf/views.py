b29424369ea2195ba6f9abc1176d40f6b4ac2db1
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-05 17:50:44 +0200

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,19 +2,17 @@ from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.http import require_http_methods
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.exceptions import SuspiciousOperation
-from .forms import SearchForm, ExampleForm  # Correct import including ExampleForm
+from .forms import SearchForm, ExampleForm
 from .models import Book
 
 @csrf_protect
 @require_http_methods(["GET", "POST"])
 def search_books(request):
-    """Secure search view using SearchForm"""
     results = []
     form = SearchForm(request.GET or None)
     
     if form.is_valid():
-        query = form.cleaned_data['query']
-        results = Book.objects.filter(title__icontains=query)[:100]
+        results = Book.objects.filter(title__icontains=form.cleaned_data['query'])[:100]
     
     return render(request, 'bookshelf/search.html', {
         'form': form,
@@ -22,23 +20,18 @@ def search_books(request):
     })
 
 @csrf_protect
-@require_http_methods(["GET", "POST"])
 def example_form_view(request):
-    """View demonstrating ExampleForm usage"""
     if request.method == 'POST':
         form = ExampleForm(request.POST)
         if form.is_valid():
-            # Process valid form data
             return redirect('success-view')
     else:
         form = ExampleForm()
-    
-    return render(request, 'bookshelf/example_form.html', {'form': form})
+    return render(request, 'bookshelf/form_example.html', {'form': form})
 
 @csrf_protect
 @require_http_methods(["POST"])
 def book_create(request):
-    """Book creation using ExampleForm"""
     form = ExampleForm(request.POST)
     if form.is_valid():
         book = form.save(commit=False)
@@ -50,7 +43,6 @@ def book_create(request):
 @csrf_protect
 @require_http_methods(["GET", "POST"])
 def book_edit(request, pk):
-    """Book editing using ExampleForm"""
     book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
         form = ExampleForm(request.POST, instance=book)
@@ -61,9 +53,11 @@ def book_edit(request, pk):
             return redirect('book-detail', pk=book.pk)
     else:
         form = ExampleForm(instance=book)
-    
     return render(request, 'bookshelf/book_form.html', {'form': form})
 
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
 def csrf_failure(request, reason=""):
-    """Custom CSRF failure view"""
     return render(request, 'bookshelf/csrf_failure.html', status=403)
