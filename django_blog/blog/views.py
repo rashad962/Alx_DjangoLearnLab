981c869fb31d3f2016c449d0c600834915175761
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 14:51:46 +0200

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,58 +1,18 @@
 # views.py
 
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post, Comment
-from .forms import CommentForm
+from django.db.models import Q
+from django.shortcuts import render
+from .models import Post
 
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'blog/post_detail.html'
+def search(request):
+    query = request.GET.get('q')
+    if query:
+        posts = Post.objects.filter(
+            Q(title__icontains=query) | 
+            Q(content__icontains=query) | 
+            Q(tags__name__icontains=query)
+        ).distinct()
+    else:
+        posts = Post.objects.all()
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        post = self.get_object()
-        context['comments'] = post.comments.all()
-        context['comment_form'] = CommentForm()
-        return context
-
-class CommentCreateView(LoginRequiredMixin, CreateView):
-    model = Comment
-    form_class = CommentForm
-    template_name = 'blog/post_detail.html'
-
-    def form_valid(self, form):
-        post = get_object_or_404(Post, pk=self.kwargs['pk'])
-        form.instance.author = self.request.user
-        form.instance.post = post
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return self.object.get_absolute_url()
-
-class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Comment
-    fields = ['content']
-    template_name = 'blog/comment_form.html'
-
-    def form_valid(self, form):
-        return super().form_valid(form)
-
-    def test_func(self):
-        comment = self.get_object()
-        return self.request.user == comment.author
-
-    def get_success_url(self):
-        return self.object.get_absolute_url()
-
-class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Comment
-    template_name = 'blog/comment_confirm_delete.html'
-
-    def test_func(self):
-        comment = self.get_object()
-        return self.request.user == comment.author
-
-    def get_success_url(self):
-        return self.object.post.get_absolute_url()
+    return render(request, 'blog/search_results.html', {'posts': posts, 'query': query})
