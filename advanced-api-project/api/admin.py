010896bc83a43060fb83a765fca777f480195132
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 13:29:34 +0200

    Update admin.py

--- advanced-api-project/api/admin.py
+++ advanced-api-project/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+# api/admin.py
+
+from django.contrib import admin
+from .models import Author, Book
+
+admin.site.register(Author)
+admin.site.register(Book)
