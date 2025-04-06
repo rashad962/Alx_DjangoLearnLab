ab6c29b602449f3a27e553233bc97106861e2f30
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 17:31:42 +0200

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import FeedView
 
 urlpatterns = [
-    path('feed/', FeedView.as_view(), name='user_feed'),
+    path('feed/', FeedView.as_view(), name='feed'),
 ]
