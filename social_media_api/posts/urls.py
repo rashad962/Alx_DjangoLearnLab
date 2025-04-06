6979e6bb90aa3f4bc557da55e1d3784a92fe8450
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 17:52:54 +0200

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # URL pattern for liking a post
     path('<int:pk>/like/', views.LikePostView.as_view(), name='like_post'),
-    
-    # URL pattern for unliking a post
     path('<int:pk>/unlike/', views.UnlikePostView.as_view(), name='unlike_post'),
 ]
