deb4b492199335b399d0b671e687a1b2aa09e9bd
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 14:51:33 +0200

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,20 +1,11 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
-from django import forms
-
-class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ("username", "email", "password1", "password2")
-
 # forms.py
 
 from django import forms
-from .models import Comment
+from .models import Post, Tag
 
-class CommentForm(forms.ModelForm):
+class PostForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), required=False, widget=forms.CheckboxSelectMultiple)
+
     class Meta:
-        model = Comment
-        fields = ['content']
+        model = Post
+        fields = ['title', 'content', 'tags']  # Add tags field here
