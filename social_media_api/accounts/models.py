8c6281275d77383c0f30096585233cb8f134986f
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 16:53:52 +0200

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,10 +1,9 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class CustomUser(AbstractUser):
-    bio = models.TextField(null=True, blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+    # Many-to-many relationship for users following other users
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers', blank=True)
 
     def __str__(self):
         return self.username
