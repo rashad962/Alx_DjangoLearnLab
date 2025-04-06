f61c18ed6177b9de77da534456be9314d5337e74
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 14:56:57 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,18 +1,7 @@
-# urls.py
-
 from django.urls import path
-from .views import (
-    PostListView, PostDetailView, CommentCreateView, 
-    CommentUpdateView, CommentDeleteView
-)
+from . import views
 
 urlpatterns = [
-    path('posts/', PostListView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    # Add the comment creation URL under a specific post
-    path('posts/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
-    # Add the comment update URL using the comment pk
-    path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
-    # Add the comment delete URL using the comment pk
-    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('search/', views.search, name='search'),
+    path('tags/<str:tag_name>/', views.tagged_posts, name='tag-detail'),
 ]
