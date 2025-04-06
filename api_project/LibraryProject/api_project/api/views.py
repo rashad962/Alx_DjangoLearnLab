eb82310c87fc959674348e08fe248b033df3ffbf
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 09:55:03 +0200

    Update views.py

--- api_project/LibraryProject/api_project/api/views.py
+++ api_project/LibraryProject/api_project/api/views.py
@@ -1,10 +1,7 @@
-# api/views.py
-
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-# View to list and create books
 class BookListCreate(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
