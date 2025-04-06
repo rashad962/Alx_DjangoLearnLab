38d1a8ee31b2d8c2850340f59ec50081114a423b
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 17:40:39 +0200

    Update views.py

--- social_media_api/notifications/views.py
+++ social_media_api/notifications/views.py
@@ -1,35 +1,10 @@
-from rest_framework import viewsets, permissions, generics
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
-from rest_framework.permissions import IsAuthenticated
-from accounts.models import CustomUser
+from rest_framework import generics, permissions
+from .models import Notification
+from .serializers import NotificationSerializer
 
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
-    serializer_class = PostSerializer
+class NotificationListView(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+    serializer_class = NotificationSerializer
 
     def get_queryset(self):
-        return Post.objects.all().order_by('-created_at')
-
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-
-class FeedView(generics.ListAPIView):
-    serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        following_users = self.request.user.following.all()
-        # This exact line will satisfy the checker
-        return Post.objects.filter(author__in=following_users).order_by('-created_at')
+        return self.request.user.notifications.order_by('-timestamp')
