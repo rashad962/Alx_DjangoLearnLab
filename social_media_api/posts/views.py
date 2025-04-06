c6854e386708a5d642d1616ed3abb6dfd0c4723f
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 16:48:10 +0200

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,28 +1,14 @@
-from rest_framework import viewsets, permissions
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import filters
 
-# Post Viewset
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['created_at', 'title']  # Allow filtering by created_at or title
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
     def get_queryset(self):
-        return Post.objects.filter(author=self.request.user)  # Only allow posts by the current user
-
-# Comment Viewset
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-    def get_queryset(self):
-        return Comment.objects.filter(author=self.request.user)  # Only allow comments by the current user
+        return Post.objects.filter(author=self.request.user)
