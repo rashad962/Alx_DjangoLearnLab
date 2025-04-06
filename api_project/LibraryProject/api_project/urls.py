398cc719c295f33c38989e6b4c6d7c1d831e8b57
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 09:35:01 +0200

    Update urls.py

--- api_project/LibraryProject/api_project/urls.py
+++ api_project/LibraryProject/api_project/urls.py
@@ -1,28 +1,8 @@
+# api/urls.py
+
 from django.urls import path
-from django.contrib.auth import views as auth_views
-from relationship_app.views import (
-    list_books,
-    add_book,
-    edit_book,
-    delete_book,
-    author_list,
-    add_author,
-    register
-)
+from . import views
 
 urlpatterns = [
-    # Book URLs
-    path('books/', list_books, name='list_books'),
-    path('add_book/', add_book, name='add_book'),
-    path('edit_book/<int:pk>/', edit_book, name='edit_book'),
-    path('delete_book/<int:pk>/', delete_book, name='delete_book'),
-    
-    # Author URLs
-    path('authors/', author_list, name='author_list'),
-    path('authors/add/', add_author, name='add_author'),
-    
-    # Authentication URLs
-    path('register/', register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('books/', views.BookListCreate.as_view(), name='book-list-create'),
 ]
