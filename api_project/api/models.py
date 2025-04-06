31489cec9f95149c1b3593c8849b1a4aadb2a485
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 13:09:28 +0200

    Update models.py

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,3 +1,10 @@
+# api/models.py
+
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)  # Title of the book
+    author = models.CharField(max_length=200)  # Author of the book
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
