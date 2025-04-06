d8d2b3fd2e48107585dfa1343645ce48f4d89c7f
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 13:28:17 +0200

    Update models.py

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -2,9 +2,16 @@
 
 from django.db import models
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)  # Title of the book
-    author = models.CharField(max_length=200)  # Author of the book
+class Author(models.Model):
+    name = models.CharField(max_length=100, help_text="Author's name")
 
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200, help_text="Title of the book")
+    publication_year = models.IntegerField(help_text="Year the book was published")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return self.title
