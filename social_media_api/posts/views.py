f6f505a07b4e42ff5e15a05216e1e9af1e26d5f4
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 17:31:27 +0200

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,14 +1,13 @@
-from rest_framework.views import APIView
+from rest_framework import generics, permissions
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 from .models import Post
 from .serializers import PostSerializer
 
-class FeedView(APIView):
-    permission_classes = [IsAuthenticated]
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-    def get(self, request):
-        followed_users = request.user.following.all()
-        posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        # Get the users the current user is following
+        followed_users = self.request.user.following.all()
+        return Post.objects.filter(author__in=followed_users).order_by('-created_at')
