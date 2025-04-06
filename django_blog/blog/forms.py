890c9e0811a70b64305ced5879c72a3b583af1dd
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 15:04:49 +0200

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import Post
-from taggit.forms import TagField, TagWidget  # Import TagWidget
+from taggit.forms import TagField, TagWidget  # Import TagWidget and TagField
 
 class PostForm(forms.ModelForm):
     tags = TagField(widget=TagWidget())  # Apply the TagWidget to the tags field
