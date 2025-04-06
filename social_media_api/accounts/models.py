e3bc57a7250439db46fde24e14e9ff13e4064a06
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 17:06:27 +0200

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
