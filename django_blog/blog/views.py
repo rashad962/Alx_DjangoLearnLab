e1db5f900f5866dd7017e55a27c821a37916db8b
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 14:23:55 +0200

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def profile_view(request):
+    return render(request, 'blog/profile.html', {'user': request.user})
