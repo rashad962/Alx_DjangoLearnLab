529bc767449c96793a71cc6c126922b57fa87041
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 17:41:59 +0200

    Update urls.py

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include  # Make sure 'include' is imported
-from rest_framework.authtoken import views as auth_views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),  # For the browsable API
-    path('api-token-auth/', auth_views.obtain_auth_token),  # Token authentication
-    path('api/', include('posts.urls')),  # Add 'api/' prefix for posts URLs
-    path('accounts/', include('accounts.urls')),
+    path('posts/', include('posts.urls')),
+    path('notifications/', include('notifications.urls')),  # Added
 ]
-
-
