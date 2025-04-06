54891202adbf91d6d5c8f12fda0678fbf55a234b
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 14:26:33 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+afrom django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name='profile'),
 ]
+
+
