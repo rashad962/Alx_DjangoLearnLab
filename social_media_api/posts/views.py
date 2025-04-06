763eeac31180f897411c56d44d8aea3b7f0b4c4e
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 17:28:44 +0200

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,36 +1,14 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.pagination import PageNumberPagination
-from .models import Post
-
-from .serializers import PostSerializer
-from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth import get_user_model
+from .models import Post
+from .serializers import PostSerializer
 
-User = get_user_model()
-
-class UserListView(APIView):
+class FeedView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def get(self, request, *args, **kwargs):
-        users = User.objects.all()
-        users_list = [{"id": user.id, "username": user.username} for user in users]
-        return Response({"users": users_list})
-
-class FeedPagination(PageNumberPagination):
-    page_size = 10  # Adjust this to set how many posts per page
-    page_size_query_param = 'page_size'
-    max_page_size = 100
-
-class PostFeedViewSet(viewsets.ReadOnlyModelViewSet):
-    serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
-    pagination_class = FeedPagination
-
-    def get_queryset(self):
-        # Get posts from users the logged-in user is following
-        user = self.request.user
-        followed_users = user.following.all()
-        return Post.objects.filter(author__in=followed_users).order_by('-created_at')
+    def get(self, request):
+        followed_users = request.user.following.all()
+        posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
