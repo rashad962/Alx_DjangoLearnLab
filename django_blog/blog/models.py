54df877c4cbcb2c0d378db265f4302cbde4c0b53
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 14:52:54 +0200

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,7 +1,6 @@
-# models.py
-
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -14,7 +13,7 @@ class Post(models.Model):
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)  # Many-to-many relationship with Tag
+    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)
 
     def __str__(self):
         return self.title
