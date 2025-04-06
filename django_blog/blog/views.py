58f1320a8abeb1735c767b0e69695c6e6d3d96cf
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 14:43:16 +0200

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,20 +1,18 @@
+# blog/views.py
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
-# ListView: List all posts
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
     ordering = ['-created_at']
 
-# DetailView: View a single post
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
 
-# CreateView: Allow authenticated users to create posts
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
@@ -24,21 +22,19 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user  # Set the logged-in user as the author
         return super().form_valid(form)
 
-# UpdateView: Allow only the author to update a post
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
     template_name = 'blog/post_form.html'
 
     def form_valid(self, form):
-        form.instance.author = self.request.user  # Set the logged-in user as the author
+        form.instance.author = self.request.user  # Ensure the user is the author
         return super().form_valid(form)
 
     def test_func(self):
         post = self.get_object()
-        return self.request.user == post.author  # Only the author can update the post
+        return self.request.user == post.author  # Only allow author to update
 
-# DeleteView: Allow only the author to delete a post
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'blog/post_confirm_delete.html'
@@ -46,4 +42,4 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
     def test_func(self):
         post = self.get_object()
-        return self.request.user == post.author  # Only the author can delete the post
+        return self.request.user == post.author  # Only allow author to delete
