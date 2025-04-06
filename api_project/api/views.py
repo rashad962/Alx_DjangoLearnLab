3d8599d25911e8fa2d90c1cc72d4baf029ef7f33
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 13:14:56 +0200

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,7 @@
-# api/views.py
-
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()  # Get all books
-    serializer_class = BookSerializer  # Use the BookSerializer for serialization
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
