be43b060945d842f37940195a98558d1542a492b
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 17:40:57 +0200

    Update serializers.py

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -1,20 +1,12 @@
 from rest_framework import serializers
-from .models import Post, Comment
-from django.contrib.auth import get_user_model
+from .models import Post, Like
 
-# Post Serializer
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField()  # Display username of the author
-
     class Meta:
         model = Post
         fields = ['id', 'author', 'title', 'content', 'created_at', 'updated_at']
 
-# Comment Serializer
-class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField()  # Display username of the author
-    post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all())  # Link to post
-
+class LikeSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Comment
-        fields = ['id', 'author', 'post', 'content', 'created_at', 'updated_at']
+        model = Like
+        fields = ['id', 'user', 'post', 'created_at']
