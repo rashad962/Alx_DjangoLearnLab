7d6ccdbcb00d5fe5e92cbe9ca8d21cccc82f9db7
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 17:04:47 +0200

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -107,6 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 
 # Internationalization
