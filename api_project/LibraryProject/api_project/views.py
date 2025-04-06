fe3b101c59adf967ecb8a897754b5cf75693d230
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 09:34:48 +0200

    Update views.py

--- api_project/LibraryProject/api_project/views.py
+++ api_project/LibraryProject/api_project/views.py
@@ -1,68 +1,10 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import permission_required
-from django.contrib.auth.decorators import login_required, permission_required
-from django.contrib import messages
-from .models import Book, Author
-from .forms import BookForm, AuthorForm
+# api/views.py
 
-@login_required
-def list_books(request):
-    books = Book.objects.all()
-    if not request.user.has_perm('relationship_app.can_view_restricted'):
-        books = books.filter(available_copies__gt=0)
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-@login_required
-@permission_required('relationship_app.can_add_book', raise_exception=True)
-def add_book(request):
-    if request.method == 'POST':
-        form = BookForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Book added successfully!')
-            return redirect('list_books')
-    else:
-        form = BookForm()
-    return render(request, 'relationship_app/book_form.html', {'form': form, 'action': 'Add'})
-
-@login_required
-@permission_required('relationship_app.can_change_book', raise_exception=True)
-def edit_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        form = BookForm(request.POST, instance=book)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Book updated successfully!')
-            return redirect('list_books')
-    else:
-        form = BookForm(instance=book)
-    return render(request, 'relationship_app/book_form.html', {'form': form, 'action': 'Edit'})
-
-@login_required
-@permission_required('relationship_app.can_delete_book', raise_exception=True)
-def delete_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        book.delete()
-        messages.success(request, 'Book deleted successfully!')
-        return redirect('list_books')
-    return render(request, 'relationship_app/book_confirm_delete.html', {'book': book})
-
-@login_required
-def author_list(request):
-    authors = Author.objects.all()
-    return render(request, 'relationship_app/author_list.html', {'authors': authors})
-
-@login_required
-@permission_required('relationship_app.can_add_book', raise_exception=True)
-def add_author(request):
-    if request.method == 'POST':
-        form = AuthorForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Author added successfully!')
-            return redirect('author_list')
-    else:
-        form = AuthorForm()
-    return render(request, 'relationship_app/author_form.html', {'form': form, 'action': 'Add'})
+# View to list and create books
+class BookListCreate(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
