2e21484da7865d0579482b60c224a065cc13e62d
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 16:46:58 +0200

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
-from .views import RegisterView, LoginView, ProfileView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, CommentViewSet
+
+# Initialize the router
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('profile/', ProfileView.as_view(), name='profile'),
+    path('', include(router.urls)),
 ]
