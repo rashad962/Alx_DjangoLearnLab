943b161ff147e9200c1427e916099cb8e468a816
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 13:18:53 +0200

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,4 +1,7 @@
+# api/urls.py
+
 from django.urls import path, include
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 from . import views
 
@@ -7,5 +10,9 @@ router = DefaultRouter()
 router.register(r'books_all', views.BookViewSet, basename='book_all')
 
 urlpatterns = [
+    # Token authentication endpoint
+    path('auth-token/', obtain_auth_token, name='api-token-auth'),
+
+    # Other routes
     path('', include(router.urls)),
 ]
