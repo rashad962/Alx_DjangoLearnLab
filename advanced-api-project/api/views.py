a656be06ae0459b805665711e96e6d1a3cd3b879
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 13:53:40 +0200

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import IsAuthenticated, AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
