53a83641623ad6cfd36c42b6d9ddb13ff06736a3
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 09:11:11 +0200

    Update urls.py

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -2,9 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.book_list, name='book-list'),
-    path('books/search/', views.search_books, name='book-search'),
-    path('books/new/', views.book_create, name='book-create'),
-    path('books/<int:pk>/edit/', views.book_edit, name='book-edit'),
-    path('example-form/', views.example_form_view, name='example-form'),
+    path('', views.search_books, name='book_list'),
 ]
