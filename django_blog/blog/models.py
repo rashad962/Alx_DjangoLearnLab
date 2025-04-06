79c6c8cf3ecc0767db894ee92c1a082c703e98f6
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 14:51:19 +0200

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -2,29 +2,22 @@
 
 from django.db import models
 from django.contrib.auth.models import User
-from django.urls import reverse
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name='posts', blank=True)  # Many-to-many relationship with Tag
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
-
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return f"Comment by {self.author.username} on {self.post.title}"
-
-    def get_absolute_url(self):
-        return reverse('post-detail', kwargs={'pk': self.post.pk})
