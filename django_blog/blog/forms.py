44ab531c92324ea70d3019587ecb0922c526c47b
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 14:45:36 +0200

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -8,3 +8,13 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+# forms.py
+
+from django import forms
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
