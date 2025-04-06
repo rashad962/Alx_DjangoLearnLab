4b83f56f3b6536dcce356373654dbfbc5a8077a2
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 15:01:57 +0200

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,8 +1,9 @@
 from django import forms
-from .models import Post, Tag
+from .models import Post
+from taggit.forms import TagField  # Import TagField for tags
 
 class PostForm(forms.ModelForm):
-    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), required=False, widget=forms.CheckboxSelectMultiple)
+    tags = TagField()  # Tag field for Post creation and updates
 
     class Meta:
         model = Post
