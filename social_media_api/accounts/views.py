fae731a133a69810279accbd978cb38b0018e58c
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-06 16:56:10 +0200

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,35 +1,40 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import get_user_model
+from rest_framework.decorators import api_view
+from .serializers import CustomUserSerializer
 
 User = get_user_model()
 
-@api_view(['POST'])
-def follow_user(request, user_id):
-    try:
-        user_to_follow = User.objects.get(id=user_id)
-    except User.DoesNotExist:
-        return Response({'detail': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def post(self, request, user_id, *args, **kwargs):
+        try:
+            user_to_follow = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response({'detail': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
 
-    # Prevent a user from following themselves
-    if user_to_follow == request.user:
-        return Response({'detail': 'You cannot follow yourself'}, status=status.HTTP_400_BAD_REQUEST)
+        # Prevent a user from following themselves
+        if user_to_follow == request.user:
+            return Response({'detail': 'You cannot follow yourself'}, status=status.HTTP_400_BAD_REQUEST)
 
-    request.user.following.add(user_to_follow)
-    return Response({'detail': f'You are now following {user_to_follow.username}'}, status=status.HTTP_200_OK)
+        request.user.following.add(user_to_follow)
+        return Response({'detail': f'You are now following {user_to_follow.username}'}, status=status.HTTP_200_OK)
 
-@api_view(['POST'])
-def unfollow_user(request, user_id):
-    try:
-        user_to_unfollow = User.objects.get(id=user_id)
-    except User.DoesNotExist:
-        return Response({'detail': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
 
-    # Prevent a user from unfollowing themselves
-    if user_to_unfollow == request.user:
-        return Response({'detail': 'You cannot unfollow yourself'}, status=status.HTTP_400_BAD_REQUEST)
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def post(self, request, user_id, *args, **kwargs):
+        try:
+            user_to_unfollow = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response({'detail': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
 
-    request.user.following.remove(user_to_unfollow)
-    return Response({'detail': f'You have unfollowed {user_to_unfollow.username}'}, status=status.HTTP_200_OK)
+        # Prevent a user from unfollowing themselves
+        if user_to_unfollow == request.user:
+            return Response({'detail': 'You cannot unfollow yourself'}, status=status.HTTP_400_BAD_REQUEST)
+
+        request.user.following.remove(user_to_unfollow)
+        return Response({'detail': f'You have unfollowed {user_to_unfollow.username}'}, status=status.HTTP_200_OK)
