265f6d5eca4b75b38da9bd08ebfa47d6c6bf3ddc
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-10 07:02:37 +0200

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,47 +1,59 @@
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Comment, Post
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Post, Comment
+from .forms import CommentForm
 
-# View for creating a comment
-class CommentCreateView(LoginRequiredMixin, CreateView):
-    model = Comment
-    fields = ['content']
-    template_name = 'blog/comment_form.html'
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    comments = post.comments.all()
+    
+    if request.method == 'POST' and request.user.is_authenticated:
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.author = request.user
+            new_comment.save()
+            messages.success(request, 'Your comment has been added!')
+            return redirect('post_detail', pk=post.pk)
+    else:
+        comment_form = CommentForm()
+    
+    return render(request, 'blog/post_detail.html', {
+        'post': post,
+        'comments': comments,
+        'comment_form': comment_form,
+    })
 
-    def form_valid(self, form):
-        # Automatically link comment to post and current user
-        form.instance.author = self.request.user
-        form.instance.post = Post.objects.get(id=self.kwargs['post_id'])
-        return super().form_valid(form)
+@login_required
+def edit_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    if comment.author != request.user:
+        messages.error(request, "You can't edit this comment.")
+        return redirect('post_detail', pk=comment.post.pk)
+    
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Comment updated successfully!')
+            return redirect('post_detail', pk=comment.post.pk)
+    else:
+        form = CommentForm(instance=comment)
+    
+    return render(request, 'blog/edit_comment.html', {
+        'form': form,
+        'comment': comment,
+    })
 
-    def get_success_url(self):
-        # Redirect to the post detail page after creating the comment
-        return reverse_lazy('post-detail', kwargs={'pk': self.kwargs['post_id']})
-
-# View for updating a comment
-class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = Comment
-    fields = ['content']
-    template_name = 'blog/comment_form.html'
-
-    def test_func(self):
-        comment = self.get_object()
-        return comment.author == self.request.user
-
-    def get_success_url(self):
-        # Redirect to the post detail page after editing the comment
-        return reverse_lazy('post-detail', kwargs={'pk': self.get_object().post.id})
-
-# View for deleting a comment
-class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-    model = Comment
-    template_name = 'blog/comment_confirm_delete.html'
-
-    def test_func(self):
-        comment = self.get_object()
-        return comment.author == self.request.user
-
-    def get_success_url(self):
-        # Redirect to the post detail page after deleting the comment
-        return reverse_lazy('post-detail', kwargs={'pk': self.get_object().post.id})
+@login_required
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    if comment.author == request.user or request.user.is_superuser:
+        comment.delete()
+        messages.success(request, 'Comment deleted successfully!')
+    else:
+        messages.error(request, "You can't delete this comment.")
+    return redirect('post_detail', pk=post_pk)
