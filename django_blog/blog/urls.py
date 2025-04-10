2b69328e229de4a31d0798b20524a2665601e2c8
Rashad amanAllah <101161909+rashad962@users.noreply.github.com>
2025-04-10 06:56:02 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('comments/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment-edit'),
     path('comments/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
 
-    # Other URLs related to the blog (e.g., list of posts, etc.)
+    # Additional URLs related to the blog (e.g., list of posts, etc.)
 ]
